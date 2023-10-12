74e6ba254d0d7275a2984f3292a7db1d8ce4919e
Deepak Sawalka <Sawalka.d@northeastern.edu>
2023-10-12 13:47:08 -0400

    update webapp

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -8,13 +8,6 @@ describe("GET /healthz ", () => {
     const response = await request(app).get("/healthz");
     expect(response.statusCode).toBe(200);
   });
-
-
-// Add a negative test case to fail the workflow when the response is not 200
-test("It should fail with status other than 200", async () => {
-  const response = await request(app).get("/healthz");
-  expect(response.statusCode).not.toBe(200); // Fails if status code is 200
-});
 });
 
 

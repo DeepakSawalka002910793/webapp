f6f8419ac76dc9d3b0189eafd8c692bb32c4f7d6
Deepak Rajkumar Sawalka <114117187+DeepakSawalka@users.noreply.github.com>
2023-10-12 14:21:29 -0400

    Assign3latest (#30)
    * updating readme
    
    * updating yml4
    
    * updating yml5
    
    * updating yml6
    
    * update test
    
    * modified
    
    * modified
    
    * modified
    
    * update webapp
    
    * update
    
    * update webapp
    
    * modified test
    
    * test
    
    * modified test
    
    * modified test

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -3,8 +3,8 @@ const request = require("supertest");
 
 
 describe("GET /healthz ", () => {
-  test("It should respond 200", async () => {
-    //expect.assertions(1); 
+
+  test("It should respond 200", async () => {    
     const response = await request(app).get("/healthz");
     expect(response.statusCode).toBe(200);
   });

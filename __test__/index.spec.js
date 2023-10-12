012b7235e6667c185e2a78ab3ca3b588ac71a486
Deepak Sawalka <Sawalka.d@northeastern.edu>
2023-10-12 00:26:49 -0400

    modified

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -4,7 +4,6 @@ const request = require("supertest");
 
 describe("GET /healthz ", () => {
   test("It should respond 200", async () => {
-    //expect.assertions(1); 
     const response = await request(app).get("/healthz");
     expect(response.statusCode).toBe(200);
   });

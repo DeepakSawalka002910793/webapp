1831b7e2e409c3cfceedd7b47008f681eb471647
Deepak Sawalka <Sawalka.d@northeastern.edu>
2023-10-12 08:12:05 -0400

    helper

--- config/helper.js
+++ config/helper.js
@@ -1,5 +1,4 @@
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
 const db = require('./dbSetup');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -22,6 +21,104 @@ const getDecryptedCreds = (authHeader) => {
   return {eMail, pass};
 
 
+  const pAuthCheck = async (req, res, next) => {
+    //Check if auth header is present and is a basic auth header.
+    if (!req.headers.authorization || req.headers.authorization.indexOf("Basic ") === -1) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+  
+    //decode the auth header
+    let {eMail, pass} = getDecryptedCreds(req.headers.authorization);
+    const id = req?.params?.id;
+  
+    //Check if user is valid
+    let user = await validUser(eMail, pass);
+  
+    if (!eMail || !pass || !user) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    } 
+  
+     // Attach user to req object
+     req.user = user.dataValues;
+  
+    if(id) {
+      //Check if user creds match the user at id.
+      let dbCheck = await dbAssignVal(eMail, pass,id);
+      if(dbCheck) {
+          return res.status((dbCheck=='Forbidden')?403:404).json({
+            message: dbCheck,
+          });
+      } 
+    }
+  
+    next();
+  }
+  
+  
+  const dbAssignVal = async (eMailId, pass, id) => {
+    let userInfo = await db.user.findOne({where: {email:eMailId}, attributes: ['id']});
+    let assignInfo = await db.assignment.findOne({where: {id:id}, attributes:['owner_user_id']});
+    if (!assignInfo) {
+        return 'Not Found';
+    }
+  
+    if(userInfo.dataValues.id !== assignInfo.dataValues.owner_user_id) {
+      return 'Forbidden';
+    }
+  
+    return '';
+  }
+  
+  const validateEmail = (eMailId) => {
+    var reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  
+    if (eMailId.match(reg)) {
+      return true;
+    }
+  
+    return false;
+  }
+  
+  const validUser = async (eMail, pass) => {
+    // Validate the credentials and return the user information if valid
+    const user = await db.user.findOne({ where: { email: eMail }, attributes: ['id', 'email', 'password'] });
+    if (!user || !(await bcrypt.compare(pass, user.password))) {
+      return null;
+    }
+  
+    return user;  // Return user object instead of true
+  };
+  /*
+    let result = await db.user.findOne({where: {email:eMail}, attributes: ['password']});
+    if (!result?.dataValues?.password) {
+        return false;
+    }
+  
+    let passCheck = await bcrypt.compare(pass, result.dataValues.password);
+    if(!passCheck) {
+      return false;
+    }
+  
+    return true;
+  }
+  */
+  const dbCredVal = async (eMailId, pass, id) => {
+    const user = await db.user.findOne({ where: { email: eMailId }, attributes: ['id'] });
+    const assignment = await db.assignment.findOne({ where: { id }, attributes: ['owner_user_id'] });
+    
+    if (!assignment) {
+      return 'Not Found';
+    }
+  
+    if (user.id !== assignment.owner_user_id) {
+      return 'Forbidden';
+    }
+  
+    return '';  // Consider returning null or undefined to indicate no error
+  };
+  
 /*
   let result = await db.user.findOne({where: {id:id}, attributes: ['email','password']});
   if (!result) {

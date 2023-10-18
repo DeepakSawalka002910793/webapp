236ac6d26f4718f0936717032bf0efbfb5b42619
Deepak Sawalka <Sawalka.d@northeastern.edu>
2023-10-18 18:58:30 -0400

    modify

--- packer/webapp.sh
+++ packer/webapp.sh
@@ -19,40 +19,23 @@ sudo apt-get install -y nodejs
 # Install MariaDB
 sudo apt-get install -y mariadb-server
 sudo systemctl start mariadb
+sudo systemctl enable mariadb
 
 
+
+# Create the database and user, then grant permissions
 sudo mysql -u root <<EOF
--- Create user if not exists
-CREATE USER IF NOT EXISTS '${DBUSER}'@'${DBHOST}';
-
--- Change the password with ALTER command
-ALTER USER '${DBUSER}'@'${DBHOST}' IDENTIFIED BY '${DBPASS}';
-
--- Grant privileges
+use mysql;
+update user set authentication_string=PASSWORD('${DBPASS}') where user='${DBUSER}';
+GRANT ALL PRIVILEGES ON *.* TO '${DBUSER}'@'${DBHOST}' WITH GRANT OPTION;
+CREATE DATABASE IF NOT EXISTS ${DATABASE};
 GRANT ALL PRIVILEGES ON ${DATABASE}.* TO '${DBUSER}'@'${DBHOST}';
 FLUSH PRIVILEGES;
-
--- Create and use database
-CREATE DATABASE IF NOT EXISTS ${DATABASE};
-USE ${DATABASE};
-EXIT;
 EOF
-
-
-# Log in to MariaDB to create the user (if not exists), change the password, create a database, set permissions, and use the new database
-#sudo mysql -u root <<EOF
-#ALTER USER 'root'@'localhost' IDENTIFIED BY '${DBPASS}';
-#CREATE DATABASE IF NOT EXISTS ${DATABASE};
-#GRANT ALL PRIVILEGES ON ${DATABASE}.* TO 'root'@'localhost';
-#FLUSH PRIVILEGES;
-#USE ${DATABASE};
-#EXIT;
-#EOF
-
 
 #sudo mysqladmin -u ${DBUSER} password ${DBPASS}
 #mysqladmin -u ${DBUSER} --password=${DBPASS} --host=${DBHOST} --port=${DBPORT} create ${DATABASE}
-sudo systemctl enable mariadb
+sudo systemctl restart mariadb
 
 # Install unzip if not installed
 sudo apt-get install unzip -y

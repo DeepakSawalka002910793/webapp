3f139156639d4ee08417d82b33d766d74b317542
Deepak Sawalka <Sawalka.d@northeastern.edu>
2023-10-25 13:57:27 -0400

    updated

--- packer/webapp.sh
+++ packer/webapp.sh
@@ -19,20 +19,35 @@ sudo apt-get install unzip -y
 echo "Unzipping the web application"
 unzip webapp.zip -d webapp
 
+# Creating group and user for running the webapp
+echo "Creating group and adding ec2 user"
+sudo groupadd ec2-group
+sudo useradd -s /bin/false -g ec2-group ec2-user
+
+# Give ownership of the webapp directory to ec2-user
+echo "Changing ownership of the webapp directory"
+sudo chown -R ec2-user:ec2-group /home/admin/webapp
+
 # Navigate to the webapp directory and install node modules
 echo "Installing node modules"
 cd webapp
 npm install
 
-# Copy the systemd service file and start the service
-echo "Setting up and starting the webapp service"
+# Copy the systemd service file
+echo "Setting up the webapp service"
 sudo cp /home/admin/webapp/packer/webapp.service /etc/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl start webapp.service
-sudo systemctl enable webapp.service
 
+# Change ownership of the service file 
+sudo chown ec2-user:ec2-group /etc/systemd/system/webapp.service
+
+# Start the service
+echo "Starting the webapp service"
+sudo systemctl daemon-reload
+sudo systemctl enable webapp
+sudo systemctl start webapp
 
 echo "Script executed successfully!"
+
 
 
 

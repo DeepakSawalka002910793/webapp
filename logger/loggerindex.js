4e323e3162876fd6d4bd4f307a043835388224f8
Deepak Sawalka <Sawalka.d@northeastern.edu>
2023-11-06 16:33:42 -0500

    modify

--- logger/loggerindex.js
+++ logger/loggerindex.js
@@ -5,26 +5,31 @@ const { createLogger, format, transports } = winston;
 // Custom formatter function
 const customFormatter = format.printf((info) => {
   // Construct log message as object
-  const logMessage = {
+  let logMessage = {
     timestamp: info.timestamp,
     level: info.level.toUpperCase(),
-    method: info.method,
-    uri: info.uri,
-    statusCode: info.statusCode,
-    message: info.message,
+    message: info.message
   };
 
+  // Conditionally add properties if they exist
+  if (info.method) logMessage.method = info.method;
+  if (info.uri) logMessage.uri = info.uri;
+  if (info.statusCode) logMessage.statusCode = info.statusCode;
 
-  // Convert log message to string
-  return `${info.timestamp} ${info.level.toUpperCase()}: ${JSON.stringify(logMessage, null, 2)}`;
+  // Convert log message to a single line string
+  return `${logMessage.timestamp} ${logMessage.level}: ${JSON.stringify(logMessage)}`;
 });
 
 // Create the logger
 const logger = createLogger({
-  format: customFormatter,
+  format: format.combine(
+    format.timestamp({ format: 'YYYY-MM-DDTHH:mm:ss.SSSZZ' }), // timestamp format
+    customFormatter
+  ),
   transports: [
     new transports.File({ filename: `${appRoot}/logs/csye6225.log` }),
   ],
 });
 
 module.exports = logger;
+
